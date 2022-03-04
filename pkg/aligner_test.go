b128037309e18d55b194cf7c6557a97977bcf5f5
Marcus Olsson <marcus.olsson@hey.com>
2022-03-04 15:36:37 +0100

    Fix failing tests

--- pkg/aligner_test.go
+++ pkg/aligner_test.go
@@ -8,6 +8,15 @@ import (
 	"github.com/marcusolsson/grafana-ynab-datasource/pkg/ynab"
 )
 
+var emptyLabels = map[string]string{
+	"account_id":    "",
+	"account_name":  "",
+	"payee_id":      "",
+	"payee_name":    "",
+	"category_id":   "",
+	"category_name": "",
+}
+
 func TestAligner_Daily(t *testing.T) {
 	for _, tt := range []struct {
 		In  []ynab.Transaction
@@ -18,7 +27,7 @@ func TestAligner_Daily(t *testing.T) {
 				{Date: "2006-01-02", Amount: -1000},
 			},
 			Out: []Measurement{
-				{Time: time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC), Value: -1000},
+				{Time: time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC), Value: -1000, Labels: emptyLabels},
 			},
 		},
 		{
@@ -28,7 +37,7 @@ func TestAligner_Daily(t *testing.T) {
 				{Date: "2006-01-02", Amount: 250},
 			},
 			Out: []Measurement{
-				{Time: time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC), Value: 250},
+				{Time: time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC), Value: 250, Labels: emptyLabels},
 			},
 		},
 		{
@@ -38,17 +47,17 @@ func TestAligner_Daily(t *testing.T) {
 				{Date: "2006-01-05", Amount: 250},
 			},
 			Out: []Measurement{
-				{Time: time.Date(2006, time.January, 1, 0, 0, 0, 0, time.UTC), Value: 1000},
-				{Time: time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC), Value: 1000},
-				{Time: time.Date(2006, time.January, 3, 0, 0, 0, 0, time.UTC), Value: 500},
-				{Time: time.Date(2006, time.January, 4, 0, 0, 0, 0, time.UTC), Value: 500},
-				{Time: time.Date(2006, time.January, 5, 0, 0, 0, 0, time.UTC), Value: 250},
+				{Time: time.Date(2006, time.January, 1, 0, 0, 0, 0, time.UTC), Value: 1000, Labels: emptyLabels},
+				{Time: time.Date(2006, time.January, 2, 0, 0, 0, 0, time.UTC), Value: 1000, Labels: emptyLabels},
+				{Time: time.Date(2006, time.January, 3, 0, 0, 0, 0, time.UTC), Value: 500, Labels: emptyLabels},
+				{Time: time.Date(2006, time.January, 4, 0, 0, 0, 0, time.UTC), Value: 500, Labels: emptyLabels},
+				{Time: time.Date(2006, time.January, 5, 0, 0, 0, 0, time.UTC), Value: 250, Labels: emptyLabels},
 			},
 		},
 	} {
 		var alignable TimeSeriesTransactions = tt.In
 
-		out, err := Regularize(alignable, PeriodDaily, alignLast)
+		out, err := Regularize(alignable, PeriodDaily, alignLast, "last")
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -69,7 +78,7 @@ func TestAligner_Monthly(t *testing.T) {
 				{Date: "2006-01-02", Amount: -1000},
 			},
 			Out: []Measurement{
-				{Time: time.Date(2006, time.January, 1, 0, 0, 0, 0, time.UTC), Value: -1000},
+				{Time: time.Date(2006, time.January, 1, 0, 0, 0, 0, time.UTC), Value: -1000, Labels: emptyLabels},
 			},
 		},
 		{
@@ -80,15 +89,15 @@ func TestAligner_Monthly(t *testing.T) {
 				{Date: "2006-03-02", Amount: 500},
 			},
 			Out: []Measurement{
-				{Time: time.Date(2006, time.January, 1, 0, 0, 0, 0, time.UTC), Value: 250},
-				{Time: time.Date(2006, time.February, 1, 0, 0, 0, 0, time.UTC), Value: 250},
-				{Time: time.Date(2006, time.March, 1, 0, 0, 0, 0, time.UTC), Value: 500},
+				{Time: time.Date(2006, time.January, 1, 0, 0, 0, 0, time.UTC), Value: 250, Labels: emptyLabels},
+				{Time: time.Date(2006, time.February, 1, 0, 0, 0, 0, time.UTC), Value: 250, Labels: emptyLabels},
+				{Time: time.Date(2006, time.March, 1, 0, 0, 0, 0, time.UTC), Value: 500, Labels: emptyLabels},
 			},
 		},
 	} {
 		var alignable TimeSeriesTransactions = tt.In
 
-		out, err := Regularize(alignable, PeriodMonthly, alignLast)
+		out, err := Regularize(alignable, PeriodMonthly, alignLast, "last")
 		if err != nil {
 			t.Fatal(err)
 		}
